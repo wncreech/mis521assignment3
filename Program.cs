26e44ecec4dd85a1b0c832d9c292f21ae833d176
William Creech <wncreech@crimson.ua.edu>
2025-10-24 00:44:30 -0500

    resignation

--- Program.cs
+++ Program.cs
@@ -9,25 +9,22 @@ namespace Betterboxd
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<AppDbContext>(options =>
-             options.UseSqlServer(connectionString));
+                options.UseSqlite(connectionString));
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseSwagger();                        // Enable Swagger JSON endpoint
-                app.UseSwaggerUI(c =>                     // Enable Swagger UI
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Betterboxd API V1");
                 });
@@ -43,11 +40,13 @@ namespace Betterboxd
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
             using (var scope = app.Services.CreateScope())
             {
                 var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                db.Database.Migrate();
+                db.Database.EnsureCreated();
             }
+
             app.Run();
         }
     }
